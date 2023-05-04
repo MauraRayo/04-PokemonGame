4ed9054861e6b0b0508faf01e45e86035ad1b031
Maurania Rayo <56032847+MauraRayo@users.noreply.github.com>
2023-05-04 19:57:43 +0200

    pokemonOptions&PokemonsPicture

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+ 
+<pokemonpages />
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import pokemonpages from './pages/pokemonpages.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    pokemonpages
   }
 }
 </script>
